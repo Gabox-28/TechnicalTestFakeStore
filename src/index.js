9ecfc15e62ca565c942621181d4961771901b22e
Gabriel Mayorga <gabrielmayorgate@gmail.com>
2023-08-29 17:24:21 -0600

    Primer problema resuelto parcialmente: falta el intersection observer

--- src/index.js
+++ src/index.js
@@ -2,11 +2,31 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
 
+function SetNewOffset(newOffset){
+  localStorage.setItem('offset', newOffset)
+}
+
+function GetOffset(){
+  let newOffset = localStorage.getItem('offset')
+
+  if (newOffset){
+    newOffset = parseInt(newOffset) + 10
+  } else {
+      newOffset = 5
+  }
+
+  SetNewOffset(newOffset)
+  return newOffset
+}
+
 const getData = api => {
-  fetch(api)
+  const currentOffset = GetOffset()
+
+  fetch(`${api}?offset=${currentOffset}&limit=10`)
     .then(response => response.json())
     .then(response => {
       let products = response;
+        console.log('Hola:', products)
       let output = products.map(product => {
         // template
       });
@@ -28,4 +48,5 @@ const intersectionObserver = new IntersectionObserver(entries => {
   rootMargin: '0px 0px 100% 0px',
 });
 
+loadData()
 intersectionObserver.observe($observe);
